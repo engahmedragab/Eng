83873686198ea0c8ebc24cdb5805f7ae419b1037
Ahmed Ragab <Ahmed.Ragab@path.local>
2018-12-04 12:35:26 +0200

    frist C# test

---
+++ dotnet/pathtest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pathtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mange_trime = "ahmedwanttotrimthescript";
+            string result = mange_trime.Replace("the","ragab");
+            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine(result);
+        }
+    }
+}
